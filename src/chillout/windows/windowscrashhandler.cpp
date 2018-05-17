248b1754a1b130f028e074dd68ab99ca3af0969e
Taras Kushnir <kushnirTV@gmail.com>
2018-05-17 22:09:01 +0300

    Enable assert to file reporting

--- src/chillout/windows/windowscrashhandler.cpp
+++ src/chillout/windows/windowscrashhandler.cpp
@@ -566,10 +566,10 @@ void WindowsCrashHandler::SetProcessExceptionHandlers()
 
     
     _CrtSetReportHook2(_CRT_RPTHOOK_INSTALL, CrtReportHook);
-    //if ( !IsDebuggerPresent() ) {
-    //    _CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG );
-    //    _CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDERR );
-    //}
+    if ( !IsDebuggerPresent() ) {
+        _CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG );
+        _CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDERR );
+    }
 
     // Catch an abnormal program termination
     m_prevSigABRT = signal(SIGABRT, SigabrtHandler);
