45ed2ea70620b08ebac33a5496c6ce98a25782a6
Taras Kushnir <kushnirTV@gmail.com>
2018-05-21 07:55:33 +0000

    Tweaks for macOS

--- src/chillout/posix/posixcrashhandler.cpp
+++ src/chillout/posix/posixcrashhandler.cpp
@@ -17,7 +17,7 @@
 #define MAX_ENTRY_LENGTH 2048
 
 const char s_MangledSymbolPrefix[] = "_Z";
-const char s_SymbolCharacters[] = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const char s_MangledSymbolCharacters[] = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 struct FreeDeleter {
   void operator()(void* ptr) const {
@@ -25,48 +25,41 @@ struct FreeDeleter {
   }
 };
 
+char *fake_alloc(char **memory, size_t size) {
+    char *allocated = *memory;
+    char *last = allocated + size;
+    *last = '\0';
+    *memory += size + 1;
+    return allocated;
+}
+
 #ifdef DARWIN
 bool demangleLine(char *line, char *memory) {
-
-
-    char functionSymbol[1024] = {};
-    char moduleName[1024] = {};
+    char *functionSymbol = fake_alloc(&memory, 1024);
+    char *moduleName = fake_alloc(&memory, 1024);
     int offset = 0;
-    char addr[48] = {};
-
-    const int lineLength = strlen(line);
-
-    char *mangledStart = strstr(line, s_MangledSymbolPrefix);
-    if (mangledStart == NULL) { return false; }
-
-    size_t nameLength = strspn(mangledStart, s_SymbolCharacters);
-    if (nameLength <= 0) { nameLength = lineLength - (mangledStart - line + 1); }
-    char *mangledEnd = mangledStart + nameLength;
+    char *addr = fake_alloc(&memory, 64);
 
     // split the string, take out chunks out of stack trace
     // we are primarily interested in module, function and address
-    sscanf(strs[i], "%*s %s %s %s %*s %d", &moduleName[0], &addr[0], &functionSymbol[0], &offset);
+    sscanf(strs[i], "%*s %s %s %s %*s %d", moduleName, addr, functionSymbol, &offset);
 
-        int validCppName = 0;
-        //  if this is a C++ library, symbol will be demangled
-        //  on success function returns 0
-        char* functionName = abi::__cxa_demangle(functionSymbol, NULL, 0, &validCppName);
-
-        char stackFrame[4096] = {};
-        if (validCppName == 0) {
-            // success
-            sprintf(stackFrame, "(%s)\t0x%s — %s + %d\n", moduleName, addr, functionName, offset);
-        } else {
-            //  in the above traceback (in comments) last entry is not
-            //  from C++ binary, last frame, libdyld.dylib, is printed
-            //  from here
-            sprintf(stackFrame, "(%s)\t0x%s — %s + %d\n",
-                    moduleName, addr, functionName, offset);
-        }
-
-        if (functionName) {
-            free(functionName);
-        }
+    int validCppName = 0;
+    //  if this is a C++ library, symbol will be demangled
+    //  on success function returns 0
+    std::unique_ptr<char, FreeDeleter> functionName(abi::__cxa_demangle(functionSymbol, NULL, 0, &validCppName));
+    
+    char *stackFrame = fake_alloc(&memory, 4096);
+    if (validCppName == 0) {
+        // success
+        sprintf(stackFrame, "(%s)\t0x%s — %s + %d\n", moduleName, addr, functionName, offset);
+    } else {
+        //  in the above traceback (in comments) last entry is not
+        //  from C++ binary, last frame, libdyld.dylib, is printed
+        //  from here
+        sprintf(stackFrame, "(%s)\t0x%s — %s + %d\n",
+                moduleName, addr, functionName, offset);
+    }
 }
 #else
 bool demangleLine(char *line, char *memory) {
@@ -74,10 +67,9 @@ bool demangleLine(char *line, char *memory) {
 }
 #endif
 
-void walkStackTrace(const std::function<void(const char * const)> &callback, char *memory, unsigned int maxFrames = 127)
-{
-    void **trace = reinterpret_cast<void**>(memory);
-    memory += (maxFrames + 2) * sizeof(void*);
+void walkStackTrace(const std::function<void(const char * const)> &callback, char *memory, unsigned int maxFrames = 127) {
+    const size_t framesSize = (maxFrames + 2) * sizeof(void*);
+    void **trace = reinterpret_cast<void**>(fake_alloc(&memory, framesSize));
     int frames = backtrace(trace, maxFrames + 2);
 
     const int stackOffset = trace[2] == trace[1] ? 2 : 1;
@@ -89,6 +81,8 @@ void walkStackTrace(const std::function<void(const char * const)> &callback, cha
         char* traceLine = symbols.get()[i];
         if (demangleLine(traceLine, memory)) {
             callback(memory);
+        } else {
+            callback(traceLine);
         }
     }
 }
@@ -168,3 +162,5 @@ void PosixCrashHandler::setCrashCallback(const std::function<void()> &callback) 
 void PosixCrashHandler::setBacktraceCallback(const std::function<void(const char * const)> &callback) {
     m_BacktraceCallback = callback;
 }
+
+
